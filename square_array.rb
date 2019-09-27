6ef0e026d22368473d806144ce97a7b2b93fe4a7
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-27 18:24:56 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  array_of_squares = []
+  array.each do |number|
+    square = number * number
+    array_of_squares << square 
+  end
+  array_of_squares
 end
